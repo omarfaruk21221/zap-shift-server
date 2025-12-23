8213f3c8190da163d059b6270285d9d6e4d0616d
omarfarukcodes69 <omarfaruk.codes69@gmail.com>
2025-12-24 00:05:05 +0600

    commend await

--- index.js
+++ index.js
@@ -69,7 +69,7 @@ const client = new MongoClient(uri, {
 async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
-        await client.connect();
+        // await client.connect();
         // ====create Database =====
         const db = client.db("zpa_shift_DB");
         const parcelCollection = db.collection("parcels");
@@ -472,7 +472,7 @@ async function run() {
 
 
 
-        await client.db("admin").command({ ping: 1 });
+        // await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
     } finally {
         // Ensures that the client will close when you finish/error
